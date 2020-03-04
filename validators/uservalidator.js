38979df832d275f98d23ad3c27a3e8a40bd1078e
jowyy05 <60644745+jowyy05@users.noreply.github.com>
2020-03-04 09:38:53 +0100

    codigo cambiado

--- validators/uservalidator.js
+++ validators/uservalidator.js
@@ -2,8 +2,8 @@ const { check} = require('express-validator');
 
 const USERVALIDATOREQUIRE=[
     check('email').normalizeEmail().isEmail(),
-    check('password').isLength({ min: 5, max: 10 }),
-    check('name').isLength({ min: 3, max: 10 }),
+    check('password').isAlphanumeric().isLength({ min: 5, max: 10 }),
+    check('name').isAlpha().isLength({ min: 3, max: 10 }),
 ];
 
 const USERVALIDATOLOGIN=[
